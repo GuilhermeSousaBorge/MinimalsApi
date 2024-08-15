05f774e7a062244e97820fa2c0a07cff8da2a65a
GuilhermeSousaBorge <guilhermepllay157@gmail.com>
2024-08-14 22:30:14 -0300

    Feat: Add initial database configurations and migrations

---
+++ Infrastructure/Db/Dbcontext.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Domain.Entities;
+
+namespace MinimalApi.Infrastructure
+{
+    public class Dbcontext : DbContext
+    {
+
+        private readonly IConfiguration _configurationAppSettings;
+
+        public Dbcontext(IConfiguration configurationAppSettings)
+        {
+            _configurationAppSettings = configurationAppSettings;
+        }
+        public DbSet<Administrator> Administrators { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = _configurationAppSettings.GetConnectionString("mysql")?.ToString();
+                if (!string.IsNullOrEmpty(connectionString))
+                {
+                    optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
 using MinimalApi.DTO;
+using MinimalApi.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<Dbcontext>(options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("mysql"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql")));
+});
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapPost("/login", (LoginDTO loginDTO) => {
-    if(loginDTO.Email == "admin@teste.com" && loginDTO.Password == "123456") return Results.Ok("Login efetuado com sucesso!");
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "admin@teste.com" && loginDTO.Password == "123456") return Results.Ok("Login efetuado com sucesso!");
     return Results.Unauthorized();
 });
 
